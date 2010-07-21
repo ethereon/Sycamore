9baede31cc144ba69f7ed44b47591aa7220e5bb5
Saumitro Dasgupta <ethereon@gmail.com>
2010-07-22 00:44:56 +0530

    Fixed indentation.

--- BotController.py
+++ BotController.py
@@ -22,7 +22,6 @@ import time
 
 class BotController:
 
-
 	def __init__(self):
 		
 		#The default baudrate for the iRobot Create
@@ -38,61 +37,63 @@ class BotController:
 		
 		self.pauseDuration = 0.05
 
+
 	def connect(self, devPath):
-	    self.dev = serial.Serial(devPath, self.baudRate)
-	    self.isConnected = self.dev.isOpen()
+		self.dev = serial.Serial(devPath, self.baudRate)
+		self.isConnected = self.dev.isOpen()
 
 	def sendCommand(self, opcode, data=None):
 
-	    assert self.isConnected
-	    packet = opcode
+		assert self.isConnected
+		packet = opcode
 
-	    if data!=None:
-		    packet += data
+		if data!=None:
+			packet += data
         
-	    self.dev.write(packet)
+		self.dev.write(packet)
 
-	    if self.pauseAfterCommand:
-		    time.sleep(self.pauseDuration)
+		if self.pauseAfterCommand:
+			time.sleep(self.pauseDuration)
 
 	def start(self):
-	    self.sendCommand(OPCODE_START)
+		self.sendCommand(OPCODE_START)
 
 	def enterPassiveMode(self):
-	    self.sendCommand(OPCODE_PASSIVE_MODE)
+		self.sendCommand(OPCODE_PASSIVE_MODE)
 
 	def enterFullMode(self):
-	    self.sendCommand(OPCODE_FULL_MODE)
+		self.sendCommand(OPCODE_FULL_MODE)
 
 	def enterSafeMode(self):
-	    self.sendCommand(OPCODE_SAFE_MODE)
+		self.sendCommand(OPCODE_SAFE_MODE)
 
 	def setMode(self, mode):
 
-	    if mode not in BOT_MODES:
-		    raise ValueError('Invalid mode specified')
+		if mode not in BOT_MODES:
+			raise ValueError('Invalid mode specified')
 
-	    self.sendCommand(mode)
+		self.sendCommand(mode)
 
 	def drive(self, velocity, radius):
 
-	    if (velocity<BOUND_VELOCITY_LOWER) or (velocity>BOUND_VELOCITY_UPPER):
-		    raise ValueError('Velocity must be between %d to %d mm/s'%(BOUND_VELOCITY_LOWER, BOUND_VELOCITY_UPPER))
+		if (velocity<BOUND_VELOCITY_LOWER) or (velocity>BOUND_VELOCITY_UPPER):
+			raise ValueError('Velocity must be between %d to %d mm/s'%(BOUND_VELOCITY_LOWER, BOUND_VELOCITY_UPPER))
 
-	    if ((radius<BOUND_RADIUS_LOWER) or (radius>BOUND_RADIUS_UPPER)) and (radius!=RADIUS_DRIVE_STRAIGHT):
-		    raise ValueError('Radius must be between %d to %d mm'%(BOUND_RADIUS_LOWER, BOUND_RADIUS_UPPER))
+		if ((radius<BOUND_RADIUS_LOWER) or (radius>BOUND_RADIUS_UPPER)) and (radius!=RADIUS_DRIVE_STRAIGHT):
+			raise ValueError('Radius must be between %d to %d mm'%(BOUND_RADIUS_LOWER, BOUND_RADIUS_UPPER))
 
     
-	    kinematicDesc = struct.pack('>hh',velocity, radius)
+		kinematicDesc = struct.pack('>hh',velocity, radius)
     
-	    self.sendCommand(OPCODE_DRIVE, kinematicDesc)
+		self.sendCommand(OPCODE_DRIVE, kinematicDesc)
 
 	def rotate(self, velocity):
-	    self.drive(velocity, RADIUS_ROTATE_COUNTER_CLOCKWISE)
+		self.drive(velocity, RADIUS_ROTATE_COUNTER_CLOCKWISE)
 
 	def driveStraight(self, velocity):
-	    self.drive(velocity, RADIUS_DRIVE_STRAIGHT);
+		self.drive(velocity, RADIUS_DRIVE_STRAIGHT);
 
 
 	def brake(self):
-	    self.sendCommand(OPCODE_DRIVE_DIRECT, '\x00\x00\x00\x00')
+		self.sendCommand(OPCODE_DRIVE_DIRECT, '\x00\x00\x00\x00')
+		
